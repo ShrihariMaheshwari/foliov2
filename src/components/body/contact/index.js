6247885901c9f3fd8ee0daa17440d17440c4340a
Shrihari Maheshwari <shriharimaheshwari@Shriharis-MacBook-Air.local>
2023-04-18 14:49:17 +0530

    added google analytics

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -9,7 +9,7 @@ function Contact() {
       <label className="section-title">Contact</label>
       <div className="contact-container">
         <div className="contact-left">
-          <p>Want to get in touch? Contact me on any of the platform</p>
+          <p>Want to get in touch? Contact me on any of the platforms</p>
           <SocialContact />
         </div>
         <div className="download">
