20a1b4e69f5ea4eff22816b2eb369cb02839865b
Shrihari Maheshwari <shriharimaheshwari@Shriharis-MacBook-Air.local>
2023-04-27 11:54:36 +0530

    added projects

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,7 +3,7 @@ export const ProjectData = [
         id:1,
         title: "ChatGPT Clone",
         about: "This project is a clone of OpenAI ChatGPT. It uses the OpenAI API to communicate the user's query to the server and sends the response back to the user.",
-        tags: ["ReactJS", "NodeJS", "HTML", "CSS", "OpenAI","MongoDB"],
+        tags: ["ReactJS", "NodeJS", "HTML", "CSS", "OpenAI","MongoDB","Vercel"],
         demo:"http://maverick-ai-tool.vercel.app/",
         github: "https://github.com/ShrihariMaheshwari/Maverick-AI-Tool",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcXa6qGwplEmxvez8YXMJ0ry-zlhi9hIQoRg&usqp=CAU"
@@ -12,9 +12,18 @@ export const ProjectData = [
         id:1,
         title:'DALL.E Clone',
         about:" This project is a clone of OpenAI DALL.E. It uses the OpenAI API to communicate the user's query to the server and sends the response back to the user.",
-        tags: ["ReactJS", "NodeJS", "HTML", "CSS", "OpenAI","Cloudinary","MongoDB"],
+        tags: ["ReactJS", "NodeJS", "HTML", "CSS", "OpenAI","Cloudinary","MongoDB","Vercel"],
         demo: "https://dall-e-ai.vercel.app/",
         github: "https://github.com/ShrihariMaheshwari/dall-e-ai",
         image: "https://openaicom.imgix.net/f75255a0-4022-4781-aecb-a1d4b10fd17e/OriginalDALLE2generationofaportraitpaintingofSalvadorDaliwitharobotichalfface.jpg?fm=auto&auto=compress,format&fit=min&rect=0,0,1024,1024&w=1919&h=1919"
+    }
+    {
+        id:1,
+        title:'Article Summarizer',
+        about:" This project is an open-source project which summarizes lengthy articles into clear and concise summaries",
+        tags: ["ReactJS", "NodeJS", "HTML", "CSS", "RapidApi","Redux-Toolkit","Netlify"],
+        demo: "https://sumzzzz.netlify.app/",
+        github: "https://github.com/ShrihariMaheshwari/Summarizer",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZFgJSTxr444_xkZVeFNpCiiOtx1-SlORGQQ&usqp=CAU"
     }
 ]   
