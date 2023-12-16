02dc16576d87f74f0d0a243b9a37e908d39e7a7b
Shrihari Maheshwari <shriharimaheshwari@shriharis-air.mynetworksettings.com>
2023-12-16 12:00:55 -0500

    updated the skills section

--- src/components/body/work/work-card.js
+++ src/components/body/work/work-card.js
@@ -17,19 +17,6 @@ function WorkCard({ item }) {
           </ul>
         </div>
       </div>
-      <div className="work-info">
-        <label className="company-name">{item.company}</label>
-        <div className="work-dates">
-          <label>{item.dateJoining}</label>-<label>{item.dateEnd}</label>
-        </div>
-        <div className="work-desc">
-          <ul>
-            {item.work.map((point, index) => (
-              <li key={index}>{point}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
