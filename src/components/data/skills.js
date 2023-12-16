79a8c1e7b8a788cab10392b4edfec33585eaebe3
Shrihari Maheshwari <shriharimaheshwari@shriharis-air.mynetworksettings.com>
2023-12-16 11:08:33 -0500

    updated the skills section

--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -7,12 +7,20 @@ export const SkillsData = [
                 icon: <i class="devicon-javascript-plain colored"></i>    
             },
             {
+                name: "Typescript",
+                icon: <i class="devicon-typescript-plain colored"></i>    
+            },
+            {
                 name: "Python",
                 icon: <i class="devicon-python-plain colored"></i>         
             },
             {
                 name: "C++",
                 icon: <i class="devicon-cplusplus-plain colored"></i>        
+            }
+            {
+                name: "Java",
+                icon: <i class="devicon-java-plain colored"></i>        
             }
         ]
     },
@@ -34,6 +42,10 @@ export const SkillsData = [
             {
                 name: "Express",
                 icon: <i class="devicon-express-original colored"></i>              
+            },
+            {
+                name: "Tailwind CSS",
+                icon: <i class="devicon-tailwindcss-plain colored"></i>              
             },
             
         ]
@@ -60,6 +72,10 @@ export const SkillsData = [
             {
                 name: "Git",
                 icon: <i class="devicon-git-plain colored"></i>                 
+            },
+            {
+                name: "Figma",
+                icon: <i class="devicon-figma-plain colored"></i>                
             },
             
         ]
